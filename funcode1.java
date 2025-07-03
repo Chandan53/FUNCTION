f3e7ffb775c45302a2eb0fa20927099289dadbc0
chandan <chandankumar936672@gmail.com>
2025-07-03 12:00:52 +0530

    functioncode1

---
+++ funcode1.java
@@ -0,0 +1,11 @@
+package FUNCTION;
+
+public class funcode1 {
+    public static void print(){
+        System.out.println("hello world");
+    }
+    public static void main(String args[]){
+        print();
+    }
+    
+}
