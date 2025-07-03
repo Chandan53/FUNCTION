24158730513e4d2f4d7182ece752e9f2eac92a56
chandan <chandankumar936672@gmail.com>
2025-07-03 15:28:54 +0530

    adding two no. using function

---
+++ funcode2.java
@@ -0,0 +1,18 @@
+package FUNCTION;
+import java.util.*;
+    public class funcode2 {
+
+    public static void add_2_num(){
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int sum = a+b;
+        System.out.println("sum of a + b  = "+ sum);
+    }
+    public static void main(String args[]){
+        add_2_num();
+    }
+    
+
+    
+}
