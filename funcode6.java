b49d42e1e74484ffbabdfeb57ad5a44e3be742b9
chandan <chandankumar936672@gmail.com>
2025-07-03 17:29:51 +0530

    Concatenate string value

---
+++ funcode6.java
@@ -0,0 +1,14 @@
+package FUNCTION;
+import java.util.*;
+
+public class funcode6 {
+    public static String  concate(String first_name , String last_name){
+        String full_name = first_name + " " + last_name;
+        return full_name;
+
+    }
+    public static void main(String args[]){
+        System.out.println(concate("chandan", "kumar"));
+    }
+    
+}
