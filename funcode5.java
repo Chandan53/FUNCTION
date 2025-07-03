e2932456211ee89539456f4021c7763ac9f6e78f
chandan <chandankumar936672@gmail.com>
2025-07-03 17:21:51 +0530

    parameters & Arguments

---
+++ funcode5.java
@@ -0,0 +1,16 @@
+package FUNCTION;
+import java.util.*;
+// parameters and arguments
+
+public class funcode5 {
+    public static void subs(int a , int b){ // parameter a variable list in a function defination
+        int sub =a-b;
+        System.out.println(sub);
+    }
+    public static void main (String args[]){
+        subs(5,4); // arguments the actual value pass to the function when calling it
+
+    }
+
+    
+}
