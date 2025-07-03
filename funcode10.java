33016f61c6bfda35304d29d215613b54b59d0844
chandan <chandankumar936672@gmail.com>
2025-07-03 18:21:56 +0530

    product of two number

---
+++ funcode10.java
@@ -0,0 +1,11 @@
+package FUNCTION;
+
+public class funcode10 {
+    public static int product( int a, int b){
+        return a*b;
+    }
+    public static void main(String args[]){
+        System.out.println("product of two number is  " + product(4,5));
+    }
+    
+}
